c0afe7266ca9c55fce1a355c0f42ffbe27a9ad47
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-07-07 11:27:23 +0200

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-// We use glad to generate the appropriate functions https://github.com/Dav1dde/glad
 // We use Vulkan, as it's more performant on iOS and macOS devices, and a more modern API.
-// We use glfw for drawing the window context. 
+// We use glfw for drawing the window context.
+// + OpenXR?
 
 int main() {
     std::cout << "Hello sphere!" << std::endl;
