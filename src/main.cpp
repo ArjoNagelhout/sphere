aec62b887a92d3bf6e1ac5018d7b637a94dd3261
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-07-07 19:01:01 +0200

    managed to create a window, happy

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,41 @@
 // We use glfw for drawing the window context.
 // + OpenXR?
 
+static void glfwError(int id, const char* description)
+{
+    std::cout << description << std::endl;
+}
+
 int main() {
-    std::cout << "Hello sphere!" << std::endl;
-    return 0;
+
+    GLFWwindow* window;
+
+    glfwSetErrorCallback(&glfwError);
+    if (!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << "Wee";
+
+    window = glfwCreateWindow(640, 480, "Sphere Wee", NULL, NULL);
+
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        //std::cout << "A";
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    exit(EXIT_SUCCESS);
 }
