d0cf2bc3cfdd7b9b9641ee3080fec85965f58f41
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-07-08 02:54:08 +0200

    created instance (VK_ERROR_INCOMPATIBLE_DRIVER)

--- src/HelloWorld.h
+++ src/HelloWorld.h
@@ -1,8 +1,13 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
 
+#include <GLFW/glfw3.h>
+
+#include <string>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+
+#include <vulkan/vk_enum_string_helper.h>
 
 class HelloWorld {
 public:
@@ -13,15 +18,64 @@ public:
     }
 
 private:
+    GLFWwindow *window;
+    VkInstance instance;
+
+    const uint32_t WIDTH = 600;
+    const uint32_t HEIGHT = 400;
+
+    const char* NAME = "Sphere";
+
     void initVulkan() {
-        std::cout << "Waza";
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, NAME, nullptr, nullptr);
+
+        createInstance();
+    }
+
+    // creates a VkInstance
+    void createInstance() {
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = NAME;
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        // Get the extension count using glfw
+        uint32_t glfwExtensionsCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionsCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionsCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+        if (result != VK_SUCCESS)
+        {
+            throw std::runtime_error(string_VkResult(result));
+        }
     }
 
     void mainLoop() {
 
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+
     }
 
     void cleanup() {
-
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-
 #include "HelloWorld.h"
-
-//static void glfwError(int id, const char* description)
-//{
-//    std::cout << description << std::endl;
-//}
 
 int main()
 {
@@ -19,38 +12,4 @@ int main()
     }
 
     return EXIT_SUCCESS;
-
-//    GLFWwindow* window;
-//
-//    glfwSetErrorCallback(&glfwError);
-//    if (!glfwInit())
-//    {
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-//    window = glfwCreateWindow(640, 480, "Sphere", nullptr, nullptr);
-//    if (!window)
-//    {
-//        glfwTerminate();
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    uint32_t extensionCount = 0;
-//    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-//    std::cout << extensionCount << " extensions supported\n";
-//
-//    glm::mat4 matrix;
-//    glm::vec4 vec;
-//    auto test = matrix * vec;
-//
-//    while (!glfwWindowShouldClose(window))
-//    {
-//        glfwPollEvents();
-//    }
-//
-//    glfwDestroyWindow(window);
-//    glfwTerminate();
-//
-//    exit(EXIT_SUCCESS);
 }
