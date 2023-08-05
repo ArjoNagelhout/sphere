137b22a4e5f37a3d139088055f854f8cc4564ac5
Arjo Nagelhout <16051555+ArjoNagelhout@users.noreply.github.com>
2023-08-05 21:20:08 +0200

    added keyboard input using glfw :)

--- src/application.hpp
+++ src/application.hpp
@@ -15,6 +15,9 @@ namespace sphere {
         Application(const std::string &applicationName) {
             window = std::make_unique<renderer::Window>(applicationName, 600, 300, 200, 100);
             renderer = std::make_unique<renderer::Renderer>(*window, true);
+
+            GLFWwindow *glfwWindow = window->getWindow();
+            glfwSetKeyCallback(glfwWindow, keyCallback);
         }
 
         ~Application() {
@@ -23,9 +26,7 @@ namespace sphere {
 
         // main loop
         void run() {
-
             GLFWwindow *glfwWindow = window->getWindow();
-
             while (!glfwWindowShouldClose(glfwWindow)) {
                 glfwPollEvents();
                 renderer->update();
@@ -35,6 +36,14 @@ namespace sphere {
     private:
         std::unique_ptr<renderer::Window> window;
         std::unique_ptr<renderer::Renderer> renderer;
+
+        static void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+            if (key == GLFW_KEY_W && action == GLFW_PRESS) {
+                std::cout << "pressed key W" << std::endl;
+            } else if (key == GLFW_KEY_W && action == GLFW_RELEASE) {
+                std::cout << "released key W" << std::endl;
+            }
+        }
     };
 
 }
